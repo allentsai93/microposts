5e9dc76e27487b2373ee022ebcc1e1cda0e54e75
Allen Tsai <allentsai93@gmail.com>
2018-04-01 21:48:21 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: [
+      'babel-polyfill',
+      './src/app.js',
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js',
+  },
+  devServer: {
+    host: 'localhost', 
+    port: 3000
+  }, 
+  module: {
+    loaders: [{
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+           presets: ['env', 'stage-0']
+        }
+    }]
+  }
+}
