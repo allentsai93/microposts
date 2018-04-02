f7c9ad6b258a5a9a3c0eee7c2a4a3aa82e047ff1
Allen Tsai <allentsai93@gmail.com>
2018-04-01 23:17:08 -0700

    finished CRUD functionality, heroku deploy

--- src/app.js
+++ src/app.js
@@ -10,9 +10,15 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 // Listen for edit state
 document.querySelector('#posts').addEventListener('click', enableEdit);
 
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
+// Listen for delete
+document.querySelector('#posts').addEventListener('click',deletePost);
+
 // Get posts
 function getPosts() {
-    http.get('http://localhost:3000/posts')
+    http.get('https://fake-api-json.herokuapp.com/posts')
         .then(data => ui.showPosts(data))
         .catch(err => console.log(err));
 }
@@ -21,20 +27,38 @@ function getPosts() {
 function submitPost() {
     const title = document.querySelector('#title').value;
     const body = document.querySelector('#body').value;
+    const id = document.querySelector('#id').value;
 
     const data = {
         title,
         body
     }
 
-    // Create Post
-    http.post('http://localhost:3000/posts', data)
-        .then(data => {
-            ui.showAlert('Post added', 'alert alert-success');
-            ui.clearFields();
-            getPosts();
-        })
-        .catch(err => console.log(err))
+    // validate input
+    if(title === '' || body === ''){
+        ui.showAlert('Please fill in all fields', 'alert alert-danger')
+    } else {
+        // Check for ID
+        if(id === '') {
+        // Create Post
+            http.post('https://fake-api-json.herokuapp.com/posts', data)
+                .then(data => {
+                    ui.showAlert('Post added', 'alert alert-success');
+                    ui.clearFields();
+                    getPosts();
+                })
+                .catch(err => console.log(err))            
+        } else {
+            // Update Post
+            http.put(`https://fake-api-json.herokuapp.com/posts/${id}`, data)
+                .then(data => {
+                    ui.showAlert('Post updated', 'alert alert-success');
+                    ui.changeFormState('add');
+                    getPosts();
+                })
+                .catch(err => console.log(err))              
+        }
+    }
 }
 
 // Enable Edit State
@@ -57,3 +81,25 @@ function enableEdit(e){
 
     e.preventDefault();
 };
+
+// Delete posts
+function deletePost(e){
+    if(e.target.parentElement.classList.contains('delete')){
+    const id = e.target.parentElement.dataset.id;
+    console.log(id);
+    http.delete(`https://fake-api-json.herokuapp.com/posts/${id}`)
+    .then( message => {
+    ui.showAlert(message,'alert alert-danger');
+    getPosts();
+    })
+    .catch(err => console.log(err));
+    }
+}
+   
+// Cancel edit state
+function cancelEdit(e) {
+    if(e.target.classList.contains('post-cancel')){
+        ui.changeFormState('add');
+    }
+    e.preventDefault();
+}
--- src/ui.js
+++ src/ui.js
@@ -61,7 +61,44 @@ class UI {
     fillForm(data) {
         this.titleInput.value = data.title;
         this.bodyInput.value = data.body;
-        this.idInput.value = data.id; 
+        this.idInput.value = data.id;
+        
+        this.changeFormState('edit');
+    }
+
+    // Clear ID hidden value
+    clearIdInput(){
+        this.idInput.value = '';
+    }
+
+    // change form state
+    changeFormState(type) {
+        if(type === 'edit'){
+            this.postSubmit.textContent = 'Update Post';
+            this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+            // Create cancel button
+            const button = document.createElement('button');
+            button.className = 'post-cancel btn btn-light btn-block';
+            button.appendChild(document.createTextNode('Cancel Edit'));
+            // Get parent to insert into dom
+            const cardForm = document.querySelector('.card-form');
+            // Get element to insert before
+            const formEnd = document.querySelector('.form-end');
+            // Insert cancel button before
+            cardForm.insertBefore(button, formEnd);
+        } else {
+            this.postSubmit.textContent = 'Post It';
+            this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+            // remove cancel button if there
+            if(document.querySelector('.post-cancel')){
+                document.querySelector('.post-cancel').remove();
+            }
+            // Clear id from hidden field
+            this.clearIdInput();
+            // Clear text
+            this.clearFields();
+        }
     }
 }
 
