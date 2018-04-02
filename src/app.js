dcdae4bf67c81aa20c9a4e223036d29d8af43e6c
Allen Tsai <allentsai93@gmail.com>
2018-04-01 22:39:37 -0700

    added class modules and ui func

---
+++ src/app.js
@@ -0,0 +1,59 @@
+import { http } from './http';
+import { ui } from './ui';
+
+// Get posts on DOM load
+document.addEventListener('DOMContentLoaded', getPosts);
+
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+// Get posts
+function getPosts() {
+    http.get('http://localhost:3000/posts')
+        .then(data => ui.showPosts(data))
+        .catch(err => console.log(err));
+}
+
+// Submit post
+function submitPost() {
+    const title = document.querySelector('#title').value;
+    const body = document.querySelector('#body').value;
+
+    const data = {
+        title,
+        body
+    }
+
+    // Create Post
+    http.post('http://localhost:3000/posts', data)
+        .then(data => {
+            ui.showAlert('Post added', 'alert alert-success');
+            ui.clearFields();
+            getPosts();
+        })
+        .catch(err => console.log(err))
+}
+
+// Enable Edit State
+function enableEdit(e){
+    if(e.target.parentElement.classList.contains('edit')){
+        const id = e.target.parentElement.dataset.id;
+        const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+        const body = e.target.parentElement.previousElementSibling.textContent;
+        
+        const data = {
+            id,
+            title,
+            body
+        }
+        // Fill form with current post
+        ui.fillForm(data);
+    }
+    
+    
+
+    e.preventDefault();
+};
